4d632c3b409ff37135f26d2d0650f7dc604a6eeb
wtfroland <junebaldovino@gmail.com>
2014-01-15 17:46:32 +0800

    #R: Add base files - Add sample code from https://www.mapbox.com/mapbox.js/example/v1.0.0/dynamically-drawing-a-line/ - Use html5 boilerplate as base html

---
+++ js/airports.js
@@ -0,0 +1,4 @@
+var airports = {"airports":[
+	{"address":"Changi, Singapore","airport_type":"airport","continent":"AS","created_at":"2013-07-24T11:14:51Z","fs_id":"4f7562c6e4b0f62bd341275d","iata":"SIN","id":5994,"iso":"SG","lat":1.3514399,"lng":103.9850929,"size":"large","status":true,"title":"Singapore Changi International Airport","updated_at":"2013-07-25T13:37:22Z"},
+	{"address":"Metro Manila, Philippines","airport_type":"airport","continent":"AS","created_at":"2013-07-24T11:11:29Z","fs_id":"4b69ef23f964a520efbc2be3","iata":"MNL","id":4303,"iso":"PH","lat":14.5086111,"lng":121.0194444,"size":"large","status":true,"title":"Ninoy Aquino International Airport","updated_at":"2013-07-25T13:32:20Z"}
+]};
