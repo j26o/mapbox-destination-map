4b84eb08f1c34628e4c189b342d3a68f17ea0dd8
wtfroland <junebaldovino@gmail.com>
2014-01-17 19:48:24 +0800

    #R - Implement raphael.js and raphaelLayer

--- js/main.js
+++ js/main.js
@@ -1,31 +1,64 @@
 $(function() {
-    var map = L.mapbox.map('map', 'examples.map-9ijuk24y')
-    .setView([0, 0], 3);
-
-    // add a new line to the map, but one with no points - yet
-    var polyline = L.polyline([]).addTo(map);
-
-    // keep a tally of how many points we've added to the map
-    var pointsAdded = 0;
-
-    // start adding new points to the map
-    add();
-
-    function add() {
-
-        // addLatLng takes a new latLng location and puts it at the end of the
-        // line. You could pull points from your data or generate them - this
-        // example just makes a sine wave with some math.
-        polyline.addLatLng(
-            L.latLng(
-                Math.cos(pointsAdded / 20) * 30,
-                pointsAdded));
-
-        // pan the map along with where the line is being added. optional, of course
-        map.setView([0, pointsAdded], 3);
-
-        // then, if we haven't already added a lot of points, queue up the page
-        // to call add() in a 100 milliseconds
-        if (++pointsAdded < 360) window.setTimeout(add, 100);
-    }
+    var map = new Map('map', 'wtfroland.gn6p6aeg');
 });
+
+var Map = function (element, mapid, options){
+    var _t = this;
+
+    var defaults = {
+        animationSpeed : 700,
+        initPosition : [1.3, 103.8],
+        zoom: 11
+    };
+
+    // extend passed options and defaults
+    this.options = $.extend({}, options, defaults);
+
+    this.map = L.mapbox.map(element)
+    .setView(_t.options.initPosition, _t.options.zoom)
+    .addLayer(L.mapbox.tileLayer(mapid , {
+        detectRetina: true
+    }));
+
+    this.map.on({
+        click: function(e){
+
+            var b = new R.BezierAnim([_t.options.initPosition, e.latlng], {}, function() {
+                var p = new R.Pulse(
+                        e.latlng, 
+                        6,
+                        {'stroke': '#ffffff', 'fill': '#ffffff'}, 
+                        {'stroke': '#333333', 'stroke-width': 2});
+
+                _t.map.addLayer(p);
+                setTimeout(function() {
+                    _t.map.removeLayer(b).removeLayer(p);
+                }, 3000);
+            });
+            
+            _t.map.addLayer(b);
+        }
+    });
+
+    this.methods = {
+        getGeo : function(position) {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(_t.methods.geoSuccess, _t.methods.geoError);
+            } else {
+                console.log('not supported');
+            }
+        },
+        geoSuccess : function(position) {
+            _t.options.position = position;
+            _t.map.setView([position.coords.latitude, position.coords.longitude], _t.map.getZoom() + 1, true);
+        },
+        geoError : function(e){
+            console.log('this error occured:', e);
+        }
+    }
+
+    this.methods.getGeo();
+};
+
+var BookingMap = function(){
+};
